a686fa6367460657659f0159bd9f520c96f7c590
Rafael Vitor <barbosasaulo78@gmail.com>
2026-05-06 19:50:42 +0000

    Alteração renomeando portfolio e introduzindo condicionais no código,

--- Portfolio/Script/script.js
+++ Portfolio/Script/script.js
@@ -1,15 +1,15 @@
 const botao = document.getElementById("modoClaro/Escuro")
-var doc = document
+
 
 let claro = true;
 botao.addEventListener("click", function() {
     if (claro) {
-        doc.body.style.backgroundColor = '#141420';
-        doc.body.style.color = 'white';
+        document.body.style.backgroundColor = '#141420';
+        document.body.style.color = 'white';
         botao.textContent = "Modo Claro"
     } else { 
-        doc.body.style.backgroundColor = 'white';
-        doc.body.style.color = 'black';
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
         botao.textContent = "Modo Escuro"
     }
     claro = !claro
@@ -37,20 +37,26 @@ let diaAtual = agora.getDay()
 let anoRestante = anoFormatura - anoAtual
 if (anoRestante > 1) {
     doc.getElementById("tempoRestante").innerHTML += `Faltam ${anoRestante} anos, `
+} else if (anoRestante === 0) {
+    doc.getElementById("tempoRestante").innerHTML += `Menos de um ano, `
 } else {
     doc.getElementById("tempoRestante").innerHTML += `Faltam ${anoRestante} ano, `
 }
     let mesRestante = mesFormatura - mesAtual
     if (mesRestante > 1) {
         doc.getElementById("tempoRestante").innerHTML += `${mesRestante} meses e `
+    } else if (mesRestante === 0) {
+        doc.getElementById("tempoRestante").innerHTML += `menos de um mês e `
     } else {
         doc.getElementById("tempoRestante").innerHTML += `${mesRestante} mes e `
     }
         let diaRestante = diaFormatura - diaAtual
-        if (diaRestante > 1){
+        if (diaRestante >= 1){
             doc.getElementById("tempoRestante").innerHTML += `${diaRestante} dias.`
+        } else if (diaRestante === 0) {
+            doc.getElementById("tempoRestante").innerHTML += `menos de um dia.`
         } else {
-            doc.getElementById("tempoRestante").innerHTML += `${diaRestante} dia.`
+            doc.getElementById("tempoRestante").innerHTML += `Curso concluído`
         }
 
 
@@ -76,3 +82,20 @@ doc.getElementById("meuNome").innerHTML = NOME
 doc.getElementById("tituloProfissional").innerHTML = tituloProfissional
 doc.getElementById("minhaBio").innerHTML = minhaBio
 doc.getElementById("anoFormatura").innerHTML = anoFormatura
+
+let nota = 8.6
+let aprovado = (nota >= 6)? "aprovado" : "reprovado"
+document.write(`<p> Você está ${aprovado}</p>`)
+
+let diaSemana = agora.getDay() +1;
+switch (diaSemana) {
+    case 1: diaDeHoje = "Domingo"; break;
+    case 2: diaDeHoje = "Segunda-Feira"; break;
+    case 3: diaDeHoje = "Terça-Feira"; break;
+    case 4: diaDeHoje = "Quarta-Feira"; break;
+    case 5: diaDeHoje = "Quinta-Feira"; break;
+    case 6: diaDeHoje = "Sexta-Feira"; break;
+    case 7: diaDeHoje = "Sábado"; break;
+    default: diaDeHoje = "Dia inválido";
+}
+document.write(`<p> Hoje é dia -> ${diaDeHoje} </p>`)
